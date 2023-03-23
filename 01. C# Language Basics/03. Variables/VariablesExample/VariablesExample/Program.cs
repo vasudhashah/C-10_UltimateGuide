9a23d069e062ea881e793394f2e229c59dbee42c
vasudhashah <vasudhashahbpr@gmail.com>
2023-03-23 13:08:49 +0530

    Variables

---
+++ 01. C# Language Basics/03. Variables/VariablesExample/VariablesExample/Program.cs
@@ -0,0 +1,18 @@
+﻿class Sample
+{
+    static void Main()
+    {
+        //declare variable
+        int age = 100;
+        string studentName = "Scott";
+
+        //print value
+        System.Console.Write("Hey ");
+        System.Console.Write(studentName);
+        System.Console.Write(" , your age is ");
+        System.Console.WriteLine(age);
+
+        //wait for pressing some key
+        System.Console.ReadKey();
+    }
+}
