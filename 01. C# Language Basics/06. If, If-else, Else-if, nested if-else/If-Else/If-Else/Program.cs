ebd21269a67423368754434212487f49863d1b06
vasudhashah <vasudhashahbpr@gmail.com>
2023-03-24 15:54:54 +0530

    If-Else

---
+++ 01. C# Language Basics/06. If, If-else, Else-if, nested if-else/If-Else/If-Else/Program.cs
@@ -0,0 +1,74 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //declare variable
+        int marks = 25;
+        char gradeLetter;
+
+        //show congratulations/ failed
+        if(marks >= 35)
+        {
+            System.Console.WriteLine("Congratulations");
+        }
+        else
+        {
+            System.Console.WriteLine("Failed");
+        }
+        System.Console.WriteLine();
+
+
+        //else if
+        if(marks >= 85)
+        {
+            gradeLetter = 'O';
+        }else if(marks >= 60 && marks < 85)
+        {
+            gradeLetter = 'A';
+        }
+        else if(marks >= 50 && marks < 60)
+        {
+            gradeLetter = 'B';
+        }
+        else if(marks >= 35 && marks < 50)
+        {
+            gradeLetter = 'C';
+        }
+        else
+        {
+            gradeLetter = 'F';
+        }
+        System.Console.WriteLine(gradeLetter);
+
+        //nested Loops
+        System.Console.WriteLine();
+        if (marks >= 35)
+        {
+            if (marks >= 85)
+            {
+                gradeLetter = 'O';
+            }
+            else if (marks >= 60 && marks < 85)
+            {
+                gradeLetter = 'A';
+            }
+            else if (marks >= 50 && marks < 60)
+            {
+                gradeLetter = 'B';
+            }
+            else
+            {
+                gradeLetter = 'C';
+            }
+        }
+        else
+        {
+            gradeLetter = 'F';
+        }
+
+        System.Console.WriteLine(gradeLetter);
+
+        System.Console.ReadKey();  
+
+    }
+}
