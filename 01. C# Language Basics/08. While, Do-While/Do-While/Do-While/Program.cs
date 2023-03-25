46353acfbb8ee8ea85fd4c6f4b5175b25ed2256a
vasudhashah <vasudhashahbpr@gmail.com>
2023-03-25 18:14:15 +0530

    Do-WhileExample

---
+++ 01. C# Language Basics/08. While, Do-While/Do-While/Do-While/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //while loop - 1 to 10
+        int i = 1;
+        while(i <= 10)
+        {
+            System.Console.Write(i+" ");
+            i++;
+        }
+        System.Console.WriteLine();
+
+        
+        //while-loop 0 to 9
+        i = 0;
+        do
+        {
+            System.Console.Write(i + " ");
+            i++;
+        } while (i < 10);
+        System.Console.WriteLine();
+
+        //9 to 0
+        i = 9;
+        while (i >= 0)
+        {
+            System.Console.Write(i + " ");
+            i--;
+        }
+        System.Console.ReadKey();
+    }
+}
