d6aeb41e34d9642bfe5a3aaefc5ce8deb54bf567
vasudhashah <vasudhashahbpr@gmail.com>
2023-03-25 18:22:50 +0530

    ForLoop

---
+++ 01. C# Language Basics/09. For Loop/ForLoopExample/ForLoopExample/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //1 to 10
+        for(int i=1; i<=10; i++)
+        {
+            System.Console.Write(i+" ");
+        }
+        System.Console.WriteLine();
+
+        //nested for loop
+        for(int i=1; i <= 5; i++)
+        {
+            for(int j = 1;j <= i; j++)
+            {
+                System.Console.Write(j);
+                System.Console.Write(" ");
+            }
+            System.Console.WriteLine();
+        }
+        System.Console.ReadKey();
+    }
+}
