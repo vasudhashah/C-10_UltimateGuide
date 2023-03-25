ad964237a4fc68c3ef1e9f815e4ded0f50c85f1c
vasudhashah <vasudhashahbpr@gmail.com>
2023-03-25 17:54:59 +0530

    SwitchExample

---
+++ 01. C# Language Basics/07. Switch-Case/SwitchCaseExample/SwitchCaseExample/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //variable to store grade letter of student
+        char gradeLetter = 'A';
+        string gradeDescription;
+        switch (gradeLetter)
+        {
+            case 'O': 
+                gradeDescription = "Outstanding"; 
+                break;
+            case 'A':
+                gradeDescription = "Excellent";
+                break;
+            case 'B':
+                gradeDescription = "Good";
+                break;
+            case 'C':
+                gradeDescription = "Fail";
+                break;
+            case 'F':
+                gradeDescription = "Fail";
+                break;
+            default:
+                gradeDescription = "None";
+                break;
+        }
+        System.Console.WriteLine(gradeDescription);
+        System.Console.ReadKey();
+
+         
+    }
+}
