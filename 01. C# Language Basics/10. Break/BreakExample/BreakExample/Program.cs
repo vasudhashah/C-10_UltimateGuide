a84155edace1aa12aad73f44a52d207548638341
vasudhashah <vasudhashahbpr@gmail.com>
2023-03-26 14:08:46 +0530

    break-continue

---
+++ 01. C# Language Basics/10. Break/BreakExample/BreakExample/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //for loop 0 to 9
+        for(int i = 0; i < 10; i++) {   
+            if (i == 5)
+            {
+                break;
+            }
+            System.Console.WriteLine(i);
+        }
+        System.Console.ReadKey();
+    }
+}
