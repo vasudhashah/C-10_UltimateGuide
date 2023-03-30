e33199cbe4fb3cc218da1b2d4a7647e1d25c1711
vasudhashah <vasudhashahbpr@gmail.com>
2023-03-30 22:12:51 +0530

    Field Modifier

---
+++ 04. Fields/02. Access Modifiers of Fields/FieldsAccessModifier/ClassLibrary1/Class1.cs
@@ -0,0 +1,34 @@
+﻿public class Product
+{
+    //fields
+    private int productId;
+    protected string productName;
+    protected internal double cost;
+    public int quantityInStock;
+}
+
+//child class at same assembly
+public class DomesticProduct : Product
+{
+    public void Method1()
+    {
+        //productId = 1 not valid
+        productName = "abc"; //accessible
+        cost = 100; //accessible
+        quantityInStock = 1; //accessible
+    }
+}
+
+//Other class at same Assembly
+public class OtherClass
+{
+    public void Method1()
+    {
+        Product product = new Product();
+        //product.productID = 1; not valid
+        //product.productName = "abc"; not valid
+        product.cost = 2000; //accessible
+        product.quantityInStock = 1;
+
+    }
+}
