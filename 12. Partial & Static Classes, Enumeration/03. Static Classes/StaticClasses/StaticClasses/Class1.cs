6ddebe55243ad9c64db637d8565d4500a976739b
vasudhashah <vasudhashahbpr@gmail.com>
2023-04-21 16:58:00 +0530

    Static Classesn Enum

---
+++ 12. Partial & Static Classes, Enumeration/03. Static Classes/StaticClasses/StaticClasses/Class1.cs
@@ -0,0 +1,9 @@
+﻿public static class Country
+{
+    public static string CountryName = "India";
+    public static int NoOfStates;
+    public static int GetNoOfUnionTerritories()
+    {
+        return 8;
+    }
+}
---
+++ 12. Partial & Static Classes, Enumeration/04. Enumerations/Enumerations/Enumerations/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //create object of Person class
+        Person person = new Person();
+        person.PersonName = "John";
+        person.Email = "john@gmail.com";
+        person.AgeGroup = AgeGroupEnumeration.Adult;
+
+        Console.WriteLine(person.AgeGroup);
+        Console.ReadKey();
+    }
+}
